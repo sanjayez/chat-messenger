1fa4d30d2264bd0b9ca3b2588c12b24397210c9c
Sanjay S <sanu2336@gmail.com>
2023-04-19 08:34:29 +0000

    completed messenger

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -6,6 +6,8 @@ const streamChat = StreamChat.getInstance(
   process.env.STREAM_PRIVATE_API_KEY!
 );
 
+const TOKEN_USER_ID_MAP = new Map<string, string>()
+
 export async function userRoutes(app: FastifyInstance) {
   app.post<{ Body: { id: string; name: string; image: String } }>(
     "/signup",
@@ -36,10 +38,22 @@ export async function userRoutes(app: FastifyInstance) {
     if (user == null) return res.status(401).send();
 
     const token = streamChat.createToken(id);
-
+    TOKEN_USER_ID_MAP.set(token, user.id);
+    
     return {
       token,
       user: { name: user.name, id: user.id, image: user.image },
     };
   });
+
+  app.post<{ Body: { token: string } }>("/logout", async (req, res) => {
+    const token = req.body.token
+    if (token == null || token === "") return res.status(400).send()
+
+    const id = TOKEN_USER_ID_MAP.get(token)
+    if (id == null) return res.status(400).send()
+
+    await streamChat.revokeUserToken(id, new Date())
+    TOKEN_USER_ID_MAP.delete(token)
+  })
 }
