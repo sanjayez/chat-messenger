2d139f7f85acbbce84dc37fec5fa19290b26f126
Sanjay S <sanu2336@gmail.com>
2023-05-09 09:48:03 +0000

    import fix for stream chat

--- express-server/Routes/userRoutes.js
+++ express-server/Routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const StreamChat = require("stream-chat");
+const StreamChat = require('stream-chat').StreamChat;
 
 const streamChat = StreamChat.getInstance(
   process.env.STREAM_API_KEY,
