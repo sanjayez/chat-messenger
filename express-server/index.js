4fbb4b301e825f9ebfbbaf1f2bafebb24ac1a858
Sanjay S <sanjay.s@hitachienergy.com>
2023-05-09 14:39:48 +0530

    added express server

---
+++ express-server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
