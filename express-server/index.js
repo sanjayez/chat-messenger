fe342efec808f0f499e91a86a681d5e48e295b59
Sanjay S <sanu2336@gmail.com>
2023-05-09 09:53:30 +0000

    called user route after enabling cors

--- express-server/index.js
+++ express-server/index.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const userRoutes = require('./Routes/userRoutes');
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./Routes/userRoutes");
 
-const app = express()
-app.use('/login', userRoutes)
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+app.use("/login", userRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
-app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}`)
+);
