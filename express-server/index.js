fb89176fe3f6aae1e27b118ae60b979bd1454f00
Sanjay S <sanu2336@gmail.com>
2023-05-09 09:59:34 +0000

    fix for route nesting

--- express-server/index.js
+++ express-server/index.js
@@ -8,10 +8,7 @@ const app = express();
 
 app.use(cors());
 
-app.use("/login", userRoutes);
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+app.use("/", userRoutes);
 
 app.listen(process.env.PORT, () =>
   console.log(`listening on port ${process.env.PORT}`)
