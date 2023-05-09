1d442c1c892885d8e884c56d1237cfb3623377bd
Sanjay S <sanu2336@gmail.com>
2023-05-09 09:24:10 +0000

    added test routes for testing CORS on deployment

--- express-server/index.js
+++ express-server/index.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const userRoutes = require('./Routes/userRoutes');
+
 const app = express()
+app.use('/login', userRoutes)
 
 app.use(cors())
 
