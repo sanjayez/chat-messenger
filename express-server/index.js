b95fe55bfb99f7ef8de28b76eceb073353acddf6
Sanjay S <sanu2336@gmail.com>
2023-05-11 09:31:43 +0000

    added a logger

--- express-server/index.js
+++ express-server/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
-
 const express = require("express");
+const logger = require('morgan');
 const cors = require("cors");
 const userRoutes = require("./Routes/userRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger('dev'));
 app.use("/", userRoutes);
 
 app.listen(process.env.PORT, () =>
